48edd3b817bcfd4072dd98d1656af42409254898
Henry Paulino <corasan.dev@gmail.com>
2024-12-01 23:46:37 -0500

    update a ts interface

--- src/schema_processor/typescript.rs
+++ src/schema_processor/typescript.rs
@@ -1,3 +1,4 @@
+use super::utils;
 use crate::custom_table_def::{Column, Table};
 use std::collections::{HashMap, HashSet};
 
@@ -41,38 +42,66 @@ fn parse_typescript(content: &str) -> Vec<TypeScriptInterface> {
   interfaces
 }
 
+fn is_property_line(line: &str) -> bool {
+  let trimmed = line.trim();
+  trimmed.contains(':')
+}
+
 pub fn update_ts_interfaces(content: &str, schema: &HashMap<String, Table>) -> String {
-  let mut updated_content = content.to_string();
-  let interfaces = parse_typescript(content);
+  let mut interfaces = parse_typescript(content);
+  let mut updated_content = String::new();
 
-  println!("interfaces: {:?}", interfaces);
+  for (table_name, table) in schema {
+    let pscal_case_name = utils::to_pascal_case(table_name);
+    let interface_idx = interfaces.iter().position(|i| i.name == pscal_case_name);
 
-  // for (table_name, table) in schema {
-  //   let pscal_case_name = utils::to_pascal_case(table_name);
-  //   println!("table name: {}", pscal_case_name);
-  //   if let Some(captures) = interface_regex.captures(&updated_content) {
-  //     println!("captures: {:?}", captures);
-  //     let interface_name = captures.get(1).unwrap().as_str();
-  //     println!("interface name: {}", interface_name);
-  //     if interface_name == pscal_case_name {
-  //       println!("found interface");
-  //       let interface_content = captures.get(2).unwrap().as_str();
-  //       println!("interface content: {}", interface_content);
-  //       let mut new_interface_content = String::new();
-  //       let existing_cols: HashSet<_> = interface_content
-  //         .lines()
-  //         .filter_map(|line| {
-  //           let trimmed_line = line.trim();
-  //           if trimmed_line.ends_with(';') {
-  //             Some(trimmed_line.split(':').next().unwrap().trim().to_string())
-  //           } else {
-  //             None
-  //           }
-  //         })
-  //         .collect();
-  //       println!("existing cols: {:?}", existing_cols);
-  //     }
-  //   }
-  // }
+    if let Some(interface_idx) = interface_idx {
+      let interface = &mut interfaces[interface_idx];
+      let mut updated_lines = Vec::new();
+      let mut existing_cols = HashSet::new();
+
+      for line in &interface.content {
+        if is_property_line(line) {
+          let col_name = line.split(':').next().unwrap().trim().to_string();
+          existing_cols.insert(col_name);
+        }
+        updated_lines.push(line.clone());
+      }
+
+      for column in &table.columns {
+        let col_name = column.name.replace("\"", "");
+        let ts_type = utils::sql_to_typescript_type(&column.data_type);
+        let nullabe_suffix = if column.nullable { "?" } else { "" };
+        let new_line = format!("  {}{}: {};\n", col_name, nullabe_suffix, ts_type);
+
+        if !existing_cols.contains(&col_name) {
+          updated_lines.push(new_line);
+        }
+      }
+      interface.content = updated_lines;
+    } else {
+      let mut new_interface = TypeScriptInterface {
+        name: pscal_case_name.clone(),
+        content: Vec::new(),
+      };
+      new_interface.content.push("{".to_string());
+      for col in &table.columns {
+        let col_name = col.name.replace("\"", "");
+        let ts_type = utils::sql_to_typescript_type(&col.data_type);
+        let nullabe_suffix = if col.nullable { "?" } else { "" };
+        let new_line = format!("  {}{}: {};\n", col_name, nullabe_suffix, ts_type);
+        new_interface.content.push(new_line);
+      }
+      new_interface.content.push("}".to_string());
+      interfaces.push(new_interface);
+    }
+  }
+
+  for interface in interfaces {
+    updated_content.push_str(&format!("export interface {} {{\n", interface.name));
+    updated_content.push_str(&interface.content.join("\n"));
+    updated_content.push_str("}\n\n");
+  }
+
   updated_content
 }
